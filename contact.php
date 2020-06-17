312fdb4b0806d2e038b7144be7b8bf762360c9fb
Jeremy lee <Jeremy.lee@netmatters-scs.com>
2020-06-17 17:55:00 +0100

    first commit

---
+++ contact.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Netmatters | Challenge</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <!-- <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet"> -->
+    <link rel="stylesheet" href="css/jquery-banner.css">
+    <link rel="stylesheet" type="text/css" href="css/nmcss.css">
+</head>
+
+<body>
+    <!-- Header -->   
+    <?php
+    include(__DIR__ . '/inc/header.php');
+    ?>
+    <!-- Header -->
+                  
+    <?php
+    include(__DIR__ . '/inc/connection.php');
+    ?>
+
+
+    <!-- Footer -->   
+    <?php
+    include(__DIR__ . '/inc/footer.php');
+    ?>
+    <!-- Footer -->   
+        
+    
+    <!-- <script src="js/cookie.js"></script>
+    <script src="js/sidebar.js"></script>
+    <script src="js/header.js"></script> -->
+    <script src="https://kit.fontawesome.com/242368e4d2.js" crossorigin="anonymous"></script>
+    <script src="//code.jquery.com/jquery.min.js"></script>
+    <script src="js\jquery-banner.1.2.0.js"></script>
+</body><!-- body container -->
+
+</html>
