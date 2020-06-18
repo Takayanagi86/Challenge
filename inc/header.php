1922c5c1c98c54a82205fb8a9fcbaeb89d8f357e
Jeremy lee <Jeremy.lee@netmatters-scs.com>
2020-06-18 09:36:21 +0100

    Header finished commit

---
+++ inc/header.php
@@ -0,0 +1,26 @@
+<div class="header-container">
+    <div class="header-top">
+        <div class="top-container">
+            <div class="telephone">
+                <a href="#">T: 01953 854 006</a>
+            </div>
+            <div class="email">
+                <a href="#">E: MAIL@VPHROOFING.CO.UK</a>
+            </div>
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div class="bottom-container">
+            <div class="logo"></div>
+            <div class="menu-container">
+                <div class="roofing">Roofing</div>
+                <div class="scaffolding" text-decoration="none" color="#222f3e">Scaffolding</div>
+                <div class="reclaim">Reclaim</div>
+                <div class="about-us">About us</div>
+                <div class="testimonials">Testimonials</div>
+                <div class="gallery">Gallery</div>
+                <div class="contact-us">Contact us</div>
+            </div>
+        </div>
+    </div>
+</div>
