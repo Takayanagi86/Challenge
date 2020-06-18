562d0b4a68497db5c46faf230838e670ed3cbfed
Jeremy lee <Jeremy.lee@netmatters-scs.com>
2020-06-18 07:05:30 +0100

    Morning commit

--- index.php
+++ index.php
@@ -4,9 +4,8 @@
     <title>Netmatters | Challenge</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-    <!-- <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet"> -->
-    <link rel="stylesheet" href="css/jquery-banner.css">
+    
+    <link href='https://fonts.googleapis.com/css?family=Varela Round' rel='stylesheet'>
     <link rel="stylesheet" type="text/css" href="css/nmcss.css">
 </head>
 
@@ -15,17 +14,13 @@
     <?php
     include(__DIR__ . '/inc/header.php');
     ?>
-    <!-- Header -->
-                  
+    <!-- Header -->              
 
-        <!-- Articles -->   
-        <?php
-        include(__DIR__ . '/inc/articles.php');
-        ?>
-        <!-- Articles -->
-
-    </div><!-- main -->
-           
+    <!-- Main -->   
+    <?php
+    include(__DIR__ . '/inc/main.php');
+    ?>
+    <!-- Main -->        
 
     <!-- Footer -->   
     <?php
@@ -34,12 +29,8 @@
     <!-- Footer -->   
         
     
-    <!-- <script src="js/cookie.js"></script>
-    <script src="js/sidebar.js"></script>
-    <script src="js/header.js"></script> -->
+    <script src="js/main.js"></script>
     <script src="https://kit.fontawesome.com/242368e4d2.js" crossorigin="anonymous"></script>
-    <script src="//code.jquery.com/jquery.min.js"></script>
-    <script src="js\jquery-banner.1.2.0.js"></script>
 </body><!-- body container -->
 
 </html>
